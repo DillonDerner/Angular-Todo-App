b2980a54b860815edb1660d1387edcbdbd187c87
DillonDerner <Dillonderner@gmail.com>
2019-01-03 23:38:00 -0600

    Added Input form Added ts code to add new items, and remove items Added ts code to add new items using enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  todoArray=[]
+
+  addTodo(value){
+    this.todoArray.push(value)
+    console.log(value)
+  }
+
+  deleteTodo(value){
+    for(let i=0 ;i<= this.todoArray.length ;i++){
+      if(value == this.todoArray[i]){
+        this.todoArray.splice(i,1)
+      }
+    }
+  }
+
+  todoSubmit(value:any){
+    if(value!==""){
+      this.todoArray.push(value.todo)
+      //this.todoForm.reset()
+    }else{
+      alert('Field required **')
+    }
+    console.log(value)
+  }
+
 }
